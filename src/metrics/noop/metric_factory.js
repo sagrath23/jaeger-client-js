09a12c6eae47acf4b1ac1d4928254b20b2bbee98
Steven Augusto Villegas Castellanos <sagrath23@gmail.com>
2018-10-25 23:53:15 -0500

    add metrics definition

---
+++ src/metrics/noop/metric_factory.js
@@ -0,0 +1,17 @@
+import NoopCounter from './counter';
+import NoopTimer from './timer';
+import NoopGauge from './gauge';
+
+export default class NoopMetricFactory {
+  createCounter(name, tags) {
+    return new NoopCounter();
+  }
+
+  createTimer(name, tags) {
+    return new NoopTimer();
+  }
+
+  createGauge(name, tags) {
+    return new NoopGauge();
+  }
+}
---
+++ src/metrics/noop/timer.js
@@ -0,0 +1,3 @@
+export default class NoopTimer {
+  record(value) {}
+}
